4fb1e1ad0f5968d3869ef1df9451cea95df8b80c
Aluno <aluno@estacio.acad>
2024-08-22 19:33:50 -0300

    atualização de estrutura de todas as paginas

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ document.getElementById('convert-btn').addEventListener('click', () => {
 
     document.getElementById('result').innerText = convertedAmount;
 });
+
+function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 5000);
+}
+
+setInterval(createStar, 100);
